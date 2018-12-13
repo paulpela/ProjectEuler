b4eea6f4608855612525786d1be58c79b83b0d9c
Paweł Pela <paulpela@gmail.com>
2018-12-13 12:26:48 +0100

    Initial Commit

---
+++ ProjectEuler/ProjectEuler.h
@@ -0,0 +1,19 @@
+//
+//  ProjectEuler.h
+//  ProjectEuler
+//
+//  Created by Paweł Pela on 13/12/2018.
+//  Copyright © 2018 Paweł Pela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProjectEuler.
+FOUNDATION_EXPORT double ProjectEulerVersionNumber;
+
+//! Project version string for ProjectEuler.
+FOUNDATION_EXPORT const unsigned char ProjectEulerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProjectEuler/PublicHeader.h>
+
+
